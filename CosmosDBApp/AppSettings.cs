fec04b1023862afcd6b219ec04db7dc9bb7a214b
Vaibhao Chawale <vaibhao.chawale@perficient.com>
2020-08-17 17:49:13 -0500

    Add project files.

---
+++ CosmosDBApp/AppSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CosmosDBApp
+{
+    using Microsoft.Extensions.Configuration;
+    public class AppSettings
+    {
+        public string StorageConnectionString { get; set; }
+        public static AppSettings LoadAppSettings()
+        {
+            IConfigurationRoot configRoot = new ConfigurationBuilder()
+                .AddJsonFile("Settings.json")
+                .Build();
+            AppSettings appSettings = configRoot.Get<AppSettings>();
+            return appSettings;
+        }
+    }
+}
---
+++ CosmosDBApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CosmosDBApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Azure Cosmos Table Samples");
+            BasicSamples basicSamples = new BasicSamples();
+            basicSamples.RunSamples().Wait();
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit");
+            Console.Read();
+        }
+    }
+}
